294455cc4509b7d8678b006ca6c5f0966a303e50
rodwiltz <rodwiltz@gmail.com>
2026-06-25 14:50:30 -0500

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,12 @@
+const SCRIPT_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
 
+function saveReturnScan(data) {
+  return fetch(SCRIPT_URL, {
+    method: "POST",
+    mode: "no-cors",
+    headers: {
+      "Content-Type": "text/plain"
+    },
+    body: JSON.stringify(data)
+  });
+}
