a08e70e8deeb9c62be0f9ea2ef4c78259aafee55
rodwiltz <rodwiltz@gmail.com>
2026-06-25 14:50:49 -0500

    Update scanner.js

--- js/scanner.js
+++ js/scanner.js
@@ -1,1 +1,29 @@
+let scanner = null;
 
+function startQrScanner(onSuccess, onError) {
+  scanner = new Html5Qrcode("reader");
+
+  Html5Qrcode.getCameras()
+    .then(function(cameras) {
+      if (!cameras || cameras.length === 0) {
+        throw new Error("No camera found");
+      }
+
+      const cameraId = cameras[cameras.length - 1].id;
+
+      return scanner.start(
+        cameraId,
+        { fps: 10, qrbox: 250 },
+        onSuccess
+      );
+    })
+    .catch(function(error) {
+      onError(error);
+    });
+}
+
+function stopQrScanner() {
+  if (scanner) {
+    scanner.stop().catch(function() {});
+  }
+}
